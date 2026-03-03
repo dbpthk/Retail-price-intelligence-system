c2fc63248fb34774992beca4afb4780f6dc93a76
Dhruv <dbpthk@gmail.com>
2026-03-03 12:37:42 +1100

    fix sign in/up error

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,5 +1,8 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import { sql } from "./connection";
+import * as authSchema from "@/lib/schema";
 import * as schema from "./schema";
 
-export const db = drizzle(sql, { schema });
+export const db = drizzle(sql, {
+  schema: { ...authSchema, ...schema },
+});
